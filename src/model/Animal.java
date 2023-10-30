3976d5e93c7a9efd6ca6613db63e94b72961e6db
Guillermo849 <orivealonsoguillermo@gmail.com>
2023-10-30 16:24:19 +0100

    ADD Animal Model

---
+++ src/model/Animal.java
@@ -0,0 +1,123 @@
+package model;
+
+import java.sql.Date;
+
+public class Animal {
+	private int ID;
+	private String nombre;
+	private String especie;
+	private String raza;
+	private String sexo;
+	private int edad;
+	private long peso;
+	private String observaciones;
+	private Date fecha;
+	private String foto;
+	
+	public Animal(int iD, String nombre, String especie, String raza, String sexo, int edad, long peso,
+			String observaciones, Date fecha, String foto) {
+		ID = iD;
+		this.nombre = nombre;
+		this.especie = especie;
+		this.raza = raza;
+		this.sexo = sexo;
+		this.edad = edad;
+		this.peso = peso;
+		this.observaciones = observaciones;
+		this.fecha = fecha;
+		this.foto = foto;
+	}
+	
+	public Animal(String nombre, String especie, String raza, String sexo, int edad, long peso, String observaciones,
+			Date fecha, String foto) {
+		this.nombre = nombre;
+		this.especie = especie;
+		this.raza = raza;
+		this.sexo = sexo;
+		this.edad = edad;
+		this.peso = peso;
+		this.observaciones = observaciones;
+		this.fecha = fecha;
+		this.foto = foto;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+
+	public String getRaza() {
+		return raza;
+	}
+
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public long getPeso() {
+		return peso;
+	}
+
+	public void setPeso(long peso) {
+		this.peso = peso;
+	}
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+}
